d05663ede0fe11178539160d9cdc082baabcb839
sorenhoyer <sorenhoyer84@gmail.com>
2024-03-14 10:07:42 +0100

    feat: add api route for pdp

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const port = 3000
 
 app.get('/products', (req,res) => {
     res.status(200).json({ products });
-})
+});
+
+app.get('/products/:productId', (req,res) => {
+    res.setHeader("Content-Type", "text/html");
+    res.send(`
+        <h1>${req.params.productId}</h1>
+    `)
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+
+});
