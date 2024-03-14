814209a329c1c3cf218744f456ed4d2a5cf8537d
VERTICA\sdl <sdl@vertica.dk>
2024-03-14 11:36:26 +0100

    header initial template

--- server.js
+++ server.js
@@ -39,3 +39,33 @@ app.get('/cart', async (req,res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
+
+app.get('/mobile-menu', (req, res) => {
+    res.setHeader("Content-Type", "text/html");
+/*     const mobileMenuTemplate = `
+    <div
+    class="sm:hidden"
+    id="mobile-menu"
+    _="on closeMobileMenu add .closing then wait for animationend then remove me"
+><div class="space-y-1 px-2 pb-3 pt-2">
+    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+    <a
+        href="#"
+        class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+        aria-current="page"
+        >Frontpage</a
+    >
+    <a
+        href="#"
+        class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+        >Category</a
+    >
+</div>
+<div class="absolute z-[index: ]-1 inset-0 bg-gray-700" _="on click trigger closeMobileMenu"></div>
+
+</div>
+`; */
+
+const dummy = "<p>Hello</p>"
+    res.send(dummy);
+});
