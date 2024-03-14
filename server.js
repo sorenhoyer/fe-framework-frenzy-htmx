2df21051ed8400b8627a2819f43aeac1d8a8e57e
Rasmus Neumann <rne@vertica.dk>
2024-03-14 13:03:25 +0100

    Added search

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.get('/api/search', (req,res) => {
     res.setHeader("Content-Type", "text/html");
     const searchResultsHTML = search.results.map(result => {
         return /*html*/`
-        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-            <a href="#">
+        <div hx-boost class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+            <a href="${result.url}">
                 <img src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
                         alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
                 <div class="px-4 py-3 w-72">
